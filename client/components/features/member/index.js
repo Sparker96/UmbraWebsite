b661cda10f86fdf9142c039d208ce68dc4e8e105
Stephen Parker <sparkerr.96@gmail.com>
2023-02-23 16:20:21 -0800

    add links to single member view

--- client/components/features/member/index.js
+++ client/components/features/member/index.js
@@ -24,15 +24,49 @@ const Member = () => {
   console.log("media", media);
 
   return (
-    <>
-      {!member == null || !media == null ? (
-        <h1>Loading</h1>
-      ) : (
-        <div className="memberPage">
-          <h1>{member.name}</h1>
+    <div className="memberPage">
+      {member.name && media.assets ? (
+        <div className="memberContainer">
+          <div className="memberHeader">
+            <div className="memberSummary">
+              <div id="memberAvatar"><img src={media.assets[1].value} /></div>
+              <div id="memberDetails">
+                <ul>
+                  <li>
+                  {member.name}
+                  </li>
+                  <li>
+                  {member.character_class.name} 
+                  </li>
+                  <li>
+                  Lvl: {member.level}
+                  </li>
+                  <li>
+                  iLvl: {member.average_item_level}
+                  </li>
+                </ul>
+              </div>
+              </div>
+            <div className="memberLinks">
+              <a id="raider_io" href={`https://raider.io/characters/us/illidan/${name}`}><img src="https://cdnassets.raider.io/images/fb_app_image.jpg?2019-11-18" /></a>
+              <a id="warcraft_logs" href={`https://www.warcraftlogs.com/character/us/illidan/${name}`}><img src="https://pbs.twimg.com/profile_images/1550453257947979784/U9D70T0S_400x400.jpg" /></a>
+              <a id="wow_armory" href={`https://worldofwarcraft.blizzard.com/en-us/character/us/illidan/${name}`}><img src="https://is3-ssl.mzstatic.com/image/thumb/Purple128/v4/4d/f9/e4/4df9e494-e6be-8c34-1ef1-a81e40c0f8e0/source/512x512bb.jpg" /></a>
+            </div>
+          </div>
+          <div
+            className="memberContent"
+            // style={{
+            //   backgroundImage: `url(${media.assets[2].value})`,
+            // }}
+          >
+            <img src={media.assets[2].value} />
+          </div>
+          <div className="memberFooter"></div>
         </div>
+      ) : (
+        <h1>Loading</h1>
       )}
-    </>
+    </div>
   );
 };
 
--- client/index.js
+++ client/index.js
@@ -1,19 +1,19 @@
-import '../public/index.css';
-import React from 'react';
+import "../public/index.css";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./components/App"
-import NavBar from './components/NavBar';
+import App from "./components/App";
+import NavBar from "./components/NavBar";
 
 const root = createRoot(document.getElementById("app")); // make sure this is the same as the id of the div in your index.html
 
 root.render(
-    <Router>
-      <Provider store={store}>
-        <NavBar />
+  <Router>
+    <Provider store={store}>
+      <NavBar />
         <App />
-      </Provider>
-    </Router>
-  );
+    </Provider>
+  </Router>
+);
